9659d9c865c178621c01f3782dcd8382c2adae54
arunikax <srarfian@gmail.com>
2024-07-29 14:11:14 +0700

    feat: Popular Product Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
                     Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
                 </p>
                 <Button label="Shop Now" iconURL={arrowRight} />
+
                 <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16 ">
                     {statistics.map((stat) => (
                         <div key={stat.label}>
