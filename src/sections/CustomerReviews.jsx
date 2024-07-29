14429c00a542c7ef87e3c762fe672d5c5ea00381
arunikax <srarfian@gmail.com>
2024-07-29 19:33:51 +0700

    feat: footer

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -3,7 +3,7 @@ import { reviews } from "../constants";
 
 const CustomerReviews = () => {
     return (
-        <section className="max-container">
+        <section id="about-us" className="max-container">
             <h3 className="font-palanquin text-center text-4xl font-bold">
                 What Our <span className="text-coral-red">Customers</span> Say?
             </h3>
